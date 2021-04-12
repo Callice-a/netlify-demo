5252b3f2b95d1817e4769b0de68776f6acb7494e
Callice Austin <callice.a@gmail.com>
2021-04-12 10:23:40 -0600

    id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
-
 import {useEffect, useState} from 'react'
 
 function App() {
-  useEeffect(()=>{
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
     axios.get('https://reqres.in/api/users?page=2').then(res=>{
       setUsers(res.data.data)
     })
-  }
+  },[])
   return (
     <div className="App">
-      <h1>calllie is da bomb</h1>
-      <pre>
-        {JSON.stringify(users)}
-      </pre>
+        <h1>calllie Resume</h1>
+        <pre>
+          {JSON.stringify(users)}
+        </pre>
     </div>
   );
 }
-
 export default App;
